d56b9566c0dea95edd117abcc4784d0de1fcfccb
BuildTools <benjamin.auzanneau.tm@gmail.com>
2018-10-13 01:05:24 +0200

    Add compo in alpha

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
+import { DictionaryService } from './dictionary.service';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'beudai-translator';
+    textareaValue = '';
+    transformValue = '';
+
+    constructor(private _dictService: DictionaryService) {}
+
+    onKey(value: string) {
+        this.textareaValue = value;
+    }
+
+    transform() {
+        this.transformValue = this._dictService.frToBeudai(this.textareaValue);
+        console.log(this.transformValue);
+    }
 }
