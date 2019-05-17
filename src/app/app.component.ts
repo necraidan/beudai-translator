e25c22da26e808c0773b19f5e34051d505cc9247
BuildTools <benjamin.auzanneau.tm@gmail.com>
2019-05-17 22:33:45 +0200

    Speak out !

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,28 @@
-import { DictionaryService } from './dictionary.service';
 import { Component } from '@angular/core';
+import { DictionaryService } from './dictionary.service';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    textareaValue = '';
-    transformValue = '';
+  textareaValue = '';
+  transformValue = '';
 
-    constructor(private _dictService: DictionaryService) {}
+  constructor(private _dictService: DictionaryService) {}
 
-    onKey(value: string) {
-        this.textareaValue = value;
-    }
+  onKey(value: string) {
+    this.textareaValue = value;
+  }
 
-    transform() {
-        this.transformValue = this._dictService.frToBeudai(this.textareaValue);
-        console.log(this.transformValue);
-    }
+  transform() {
+    this.transformValue = this._dictService.frToBeudai(this.textareaValue);
+    console.log(this.transformValue);
+    this.speak(this.transformValue);
+  }
+
+  private speak(word: string) {
+    window.speechSynthesis.speak(new SpeechSynthesisUtterance(word));
+  }
 }
